195e8bb36063da0e2d094df38f20ea50ea0a8853
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-11-04 19:27:57 +0800

    update

--- sbemplms/src/main/resources/data.sql
+++ sbemplms/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO product_data (name, description, pricePerItem, stock, created, lastUpdated) VALUES
-('Product1', 'Description for Product1', 9.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Product2', 'Description for Product2', 9.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Product3', 'Description for Product3', 9.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Product4', 'Description for Product4', 9.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Product5', 'Description for Product5', 9.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('Oppenheimer Shirt', '90% cotton blend', 14.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Sublime Classic Shirt', '90% cotton blend', 9.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Chaos Reigns Shirt', '70% cotton blend', 14.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Marble Saints Shirt', '75% cotton blend', 14.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(' 対馬の侍 Shirt ', '95% cotton blend', 14.99, 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
